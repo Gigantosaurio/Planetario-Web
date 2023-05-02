6f9edce78a446c016c0a46e43bb30d2b8cfed769
Carlos Fernández Guevara <131338727+CarlosFerGue@users.noreply.github.com>
2023-05-02 12:25:35 +0200

    NavBar FINALIZADA LETSGOOOOO
    LA BESTIA LA BESTIA LA BESTIA VAMOS FAM SQUAD

--- web/index.js
+++ web/index.js
@@ -1,3 +1,24 @@
+// script para que las transiciones se vean
+
+//Con este cacho de javascript vamos a hacer que la carga de la pagina web tras darle un click
+//al hipervinculo se demore medio segundo para asi que se vea la tremenada animacion que le
+//he metido a la NavBar que mola un taco
+
+let enlacesRetrasoCarga = document.querySelectorAll('.retraso-carga');
+
+enlacesRetrasoCarga.forEach(function (enlace) {
+    enlace.addEventListener('click', function (evento) {
+        evento.preventDefault();
+        let url = this.getAttribute('href');
+        setTimeout(function () {
+            window.location = url;
+        }, 500);
+    })
+})
+
+
+// script del footer
+
 //Con esta linea loq ue hacemos es delcarar una variable bajo el nombre de list
 //que luego buscamos en el html bajo el nombre de class = "list" o .list
 const list = document.querySelectorAll('.list');
